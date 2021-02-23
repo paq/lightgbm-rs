c95c63494955e0956ebebdd91330f0e74d9b7729
paq <89paku@gmail.com>
2021-02-23 19:43:09 +0900

    wip

--- lightgbm-sys/build.rs
+++ lightgbm-sys/build.rs
@@ -3,53 +3,26 @@ extern crate cmake;
 
 use cmake::Config;
 use std::env;
-use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::path::PathBuf;
 
 fn main() {
     let target = env::var("TARGET").unwrap();
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let lgbm_root = Path::new(&out_dir).join("lightgbm");
-
-    // copy source code
-    if !lgbm_root.exists() {
-        let status = if target.contains("windows") {
-            Command::new("cmd")
-                .args(&[
-                    "/C",
-                    "echo D | xcopy /S /Y lightgbm",
-                    lgbm_root.to_str().unwrap(),
-                ])
-                .status()
-        } else {
-            Command::new("cp")
-                .args(&["-r", "lightgbm", lgbm_root.to_str().unwrap()])
-                .status()
-        };
-        if let Some(err) = status.err() {
-            panic!(
-                "Failed to copy ./lightgbm to {}: {}",
-                lgbm_root.display(),
-                err
-            );
-        }
-    }
 
     // CMake
-    let dst = Config::new(&lgbm_root)
+    let dst = Config::new("lightgbm")
         .profile("Release")
         .uses_cxx11()
         .define("BUILD_STATIC_LIB", "ON")
         .build();
 
     // bindgen build
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .clang_args(&["-x", "c++", "-std=c++11"])
-        .clang_arg(format!("-I{}", lgbm_root.join("include").display()))
+        .clang_arg(format!("-I{}", out_path.join("include").display()))
         .generate()
         .expect("Unable to generate bindings");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings.");
@@ -65,6 +38,7 @@ fn main() {
 
     println!("cargo:rustc-link-search={}", out_path.join("lib").display());
     println!("cargo:rustc-link-search=native={}", dst.display());
+
     if target.contains("windows") {
         println!("cargo:rustc-link-lib=static=lib_lightgbm");
     } else {
